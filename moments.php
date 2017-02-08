fec350d7916c38b2d85812b1171c4bbe955f6620
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2017-02-07 21:59:09 -0800

    Add files via upload

---
+++ moments.php
@@ -0,0 +1,57 @@
+<?php
+
+?>
+ <!DOCTYPE html>
+<html>
+    <head>
+       <title>Moments</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <style>
+            .container{
+                width: 500px;
+                text-align:center;
+                margin: 0 auto;
+                position:relative;
+                top: 50px;
+                font-family: sans-serif;
+                
+                font-size: 20px;
+                font-weight: bolder;
+            }
+            body{
+                background-image:url(diary.jpg);
+            }
+            h1{
+                font-size: 70px;
+                color:cornsilk;
+            }
+            p{
+                 color:floralwhite;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+        <h1>Moments</h1>
+        <p>keeps your daily thoughts under password protection and makes journaling simple and beautiful!</p>
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1"></label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="your email">
+      
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1"></label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="your password">
+        </div>
+            </form>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    </body>
+</html>
